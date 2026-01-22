52f611db20824d4b27bcbe4b1388d16978149edf
msiyem <siyem2574@gmail.com>
2026-01-22 21:07:20 +0600

    create frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-[50px] text-red-500'>
+        this is react project 
+      </div>
+    </>
+  )
+}
+
+export default App
